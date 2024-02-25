79d94bad4f11a3b92bdb440789f3d09f75e1b834
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-02-25 03:07:37 -0300

    feat: add initial files and settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './Components/Layouts/Footer';
+import Header from './Components/Layouts/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+
 const App = () => {
-  return <div>App React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
