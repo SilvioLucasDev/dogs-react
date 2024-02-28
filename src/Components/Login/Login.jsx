924ba9a021f45384ec49fc9c09df281161698084
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-02-28 01:31:37 -0300

    feat: add styles to components

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
 import LoginRecovery from './LoginRecovery';
 import LoginRestart from './LoginRestart';
-import React from 'react';
 import { UserContext } from '../../Contexts/UserContext';
+import styles from './Login.module.css';
 
 const Login = () => {
   const { login } = React.useContext(UserContext);
@@ -13,14 +14,16 @@ const Login = () => {
   if (login === true) navigate('/conta');
 
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="recuperar" element={<LoginRecovery />} />
-        <Route path="resetar" element={<LoginRestart />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="recuperar" element={<LoginRecovery />} />
+          <Route path="resetar" element={<LoginRestart />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
