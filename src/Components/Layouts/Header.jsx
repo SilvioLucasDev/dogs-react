7be78a30bf9c9fa123103b49be2d9d4ed2af504c
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-02-29 01:37:30 -0300

    refactor: adjust code of logout

--- src/Components/Layouts/Header.jsx
+++ src/Components/Layouts/Header.jsx
@@ -1,25 +1,24 @@
-import { Link } from 'react-router-dom';
-import styles from './Header.module.css';
-import Dogs from '../../Assets/dogs.svg?react';
-import React from 'react';
-import { UserContext } from '../../Contexts/UserContext';
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+import Dogs from "../../Assets/dogs.svg?react";
+import React from "react";
+import { UserContext } from "../../Contexts/UserContext";
 
 const Header = () => {
-  const { data, userLogout } = React.useContext(UserContext);
+  const { data } = React.useContext(UserContext);
 
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link className={styles.log} to={'/'} aria-label="Dogs - Home">
+        <Link className={styles.log} to={"/"} aria-label="Dogs - Home">
           <Dogs />
         </Link>
         {data ? (
-          <Link className={styles.login} to={'/conta'}>
+          <Link className={styles.login} to={"/conta"}>
             {data.nome}
-            <button onClick={userLogout}>Sair</button>
           </Link>
         ) : (
-          <Link className={styles.login} to={'/login'}>
+          <Link className={styles.login} to={"/login"}>
             Login | Criar
           </Link>
         )}
