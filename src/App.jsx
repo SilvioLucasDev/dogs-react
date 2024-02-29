01ff4327382b91c54e955d276f4737119e03cff7
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-02-29 01:38:36 -0300

    feat: add new routes of user account and implement ProtectedRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Footer from './Components/Layouts/Footer';
-import Header from './Components/Layouts/Header';
-import Home from './Components/Home';
-import Login from './Components/Login/Login';
-import { UserStorage } from './Contexts/UserContext';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Layouts/Footer";
+import Header from "./Components/Layouts/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./Contexts/UserContext";
+import User from "./Components/User/User";
+import ProtectedRouter from "./Components/Helpers/ProtectedRouter";
 
 const App = () => {
   return (
@@ -14,7 +16,15 @@ const App = () => {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRouter>
+                  <User />
+                </ProtectedRouter>
+              }
+            />
           </Routes>
           <Footer />
         </UserStorage>
