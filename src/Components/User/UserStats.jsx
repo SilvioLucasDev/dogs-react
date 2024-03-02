0662ff7f0a797aac8b5119ca4e72f9bd3a157e99
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-03-02 20:42:18 -0300

    feat: add and apply VictoryLabs to create graphs in states page

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,12 +1,33 @@
+import React from "react";
+import useFetch from "../../Hooks/useFetch";
+import { STATS_GET } from "../../api";
 import Head from "../Helpers/Head";
+import Loading from "../Helpers/Loading";
+import Error from "../Helpers/Error";
+import UserStatsGraphs from "./UserStatsGraphs";
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      User Estatísticas
-    </div>
-  );
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const token = window.localStorage.getItem("token");
+      const { url, options } = STATS_GET(token);
+      await request(url, options);
+    }
+    fetchData();
+  }, [request]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  return null;
 };
 
 export default UserStats;
